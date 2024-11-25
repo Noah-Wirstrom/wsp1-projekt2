6954e60271b71021d684f980a0fd9993f87b0911
Noah-Wirstrom <noah.wirstrom@elev.ga.ntig.se>
2024-11-25 14:41:16 +0100

    massa kod

--- app.rb
+++ app.rb
@@ -1,7 +1,31 @@
 class App < Sinatra::Base
 
-    get '/' do
+    def db
+        return @db if @db
+  
+        @db = SQLite3::Database.new("db/todos.sqlite")
+        @db.results_as_hash = true
+  
+        return @db
+    end
+   
+    get '/' do 
+        @todos = db.execute('SELECT * FROM todos')
         erb(:"index")
+    end
+
+    post '/todos/:id/delete' do | id |
+        db.execute("DELETE FROM todos WHERE id = ?" , [id])
+        redirect "/"
+    end
+
+    post '/todos/:id/:completed' do | id, completed |
+        if completed == '0'
+            db.execute("UPDATE todos SET completed == 1 WHERE id = ?", [id])
+        elsif completed == '1'
+            db.execute("UPDATE todos SET completed == 0 WHERE id = ?", [id])
+        end
+        redirect "/"
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,50 @@
 require 'sqlite3'
-
+require 'bcrypt'
 class Seeder
 
+
+
+
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
+  end
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todos')
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT,
+                completed BOOLEAN DEAFAULT FALSE)')
+
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL 
+                )')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO todos (name, description, completed) VALUES ("Städa",   "Städa hela huset", 0)')
+    db.execute('INSERT INTO todos (name, description, completed) VALUES ("Handla",    "Handla mat", 0)')
+    db.execute('INSERT INTO todos (name, description, completed) VALUES ("Sova",  "Gå å lägg dig", 0)')
+    db.execute('INSERT INTO todos (name, description, completed) VALUES ("Träna",  "BÄNK", 0)')
+    db.execute('INSERT INTO users (username, password) VALUES ("admin", "1234")')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
   end
 
 end
